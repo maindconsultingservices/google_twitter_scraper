dd0dbd2027122b2d83e94d96c96b4ac2c4016e92
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2025-02-06 23:30:48 +0100

    Create auth.py

---
+++ api/middlewares/auth.py
@@ -0,0 +1,11 @@
+from fastapi import Request, HTTPException
+from api.config.env import config
+from api.utils.logger import logger
+
+async def require_api_key(request: Request):
+    logger.debug("Auth middleware triggered for API key check.")
+    header_key = request.headers.get("x-api-key")
+    if not header_key or header_key != config.x_api_key:
+        logger.warning("Unauthorized request - invalid or missing X-API-KEY header.")
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    logger.debug("API key validated successfully.")
