9505313cac1889a7c21e81086b9ecb1013bba3f2
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2025-02-07 10:33:05 +0100

    Update config.py

--- api/config.py
+++ api/config.py
@@ -8,6 +8,9 @@ class Config:
     x_api_key = os.getenv("X_API_KEY", "")
     twitter_cookies_json = os.getenv("TWITTER_COOKIES_JSON", "")
     enable_debug = (os.getenv("ENABLE_DEBUG", "false").lower() == "true")
+    venice_api_key = os.getenv("VENICE_API_KEY", "")
+    venice_model = os.getenv("VENICE_MODEL", "")
+    venice_url = os.getenv("VENICE_URL", "")
+    venice_temperature = float(os.getenv("VENICE_TEMPERATURE", "0.2"))
 
-# Instantiate our config
 config = Config()
