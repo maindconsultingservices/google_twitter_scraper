6cb0ac895f418950bbb144e385e16179006ac9eb
maindconsultingservices <157203828+maindconsultingservices@users.noreply.github.com>
2025-03-05 15:41:51 -0600

    Update __init__.py

--- api/services/__init__.py
+++ api/services/__init__.py
@@ -2,13 +2,11 @@ from .google_service import google_service
 from .twitter_service import twitter_service
 from .web_service import web_service, email_service
 from .rate_limiter import RateLimiter
-from .linkedin_service import linkedin_service
 
 __all__ = [
     'google_service',
     'twitter_service',
     'web_service',
     'email_service',
-    'RateLimiter',
-    'linkedin_service'
+    'RateLimiter'
 ]
